83b77bc04ba16ac9d73924606a07a32d11bd4628
Mahesh <biradar.m2@gmail.com>
2017-04-30 13:47:12 +0200

    initial commit

---
+++ opportunity-management-build/opportunity-management-model/src/main/java/org/opportunity/management/model/App.java
@@ -0,0 +1,13 @@
+package org.opportunity.management.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
