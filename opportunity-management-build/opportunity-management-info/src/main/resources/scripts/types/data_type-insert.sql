cc8542304802f04628665319e42be8a2be48380c
Mahesh <biradar.m2@gmail.com>
2017-09-03 11:35:37 +0200

    added DB Scripts

---
+++ opportunity-management-build/opportunity-management-info/src/main/resources/scripts/types/data_type-insert.sql
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+--  File created - Saturday-June-10-2017   
+--------------------------------------------------------
+REM INSERTING into OPP_T_DATA_TYPE
+SET DEFINE OFF;
+Insert into OPP_T_DATA_TYPE (DATA_TYPE_ID,NAME,CONVERTER_ID) values (1,'String',1);
+Insert into OPP_T_DATA_TYPE (DATA_TYPE_ID,NAME,CONVERTER_ID) values (2,'BigDecimal',2);
+Insert into OPP_T_DATA_TYPE (DATA_TYPE_ID,NAME,CONVERTER_ID) values (3,'Boolean',4);
+Insert into OPP_T_DATA_TYPE (DATA_TYPE_ID,NAME,CONVERTER_ID) values (4,'Integer',3);
+Insert into OPP_T_DATA_TYPE (DATA_TYPE_ID,NAME,CONVERTER_ID) values (5,'Date',5);
+Insert into OPP_T_DATA_TYPE (DATA_TYPE_ID,NAME,CONVERTER_ID) values (6,'Cluster',29);
+Insert into OPP_T_DATA_TYPE (DATA_TYPE_ID,NAME,CONVERTER_ID) values (7,'Term',6);
+Insert into OPP_T_DATA_TYPE (DATA_TYPE_ID,NAME,CONVERTER_ID) values (8,'BusinessHierarchyType',104);
+Insert into OPP_T_DATA_TYPE (DATA_TYPE_ID,NAME,CONVERTER_ID) values (9,'Collector',1119);
