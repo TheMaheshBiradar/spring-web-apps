051b40e052638698a61d9b21187964d5788771b5
Mahesh <biradar.m2@gmail.com>
2017-10-09 23:32:59 +0200

    refactor controller method

--- health-check/src/main/java/com/taggy/infra/HealthCheckApplication.java
+++ health-check/src/main/java/com/taggy/infra/HealthCheckApplication.java
@@ -1,6 +1,8 @@
 package com.taggy.infra;
 
 import com.taggy.infra.model.ConfigurationStatusUrlConfiguration;
+import com.taggy.infra.service.HttpsConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +16,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 @RestController
 public class HealthCheckApplication {
+
+
+
+    @Autowired
+    HttpsConnectionFactory httpsConnectionFactory;
 
     //TODO - move this to services with transaction ready only MUST
     @PersistenceContext
@@ -25,7 +34,7 @@ public class HealthCheckApplication {
 
 
 
-    public ConfigurationStatusUrlConfiguration getUserDetails() {
+    public ConfigurationStatusUrlConfiguration getComponentDetails() {
         ConfigurationStatusUrlConfiguration config = entityManager.find(ConfigurationStatusUrlConfiguration.class
                 , new ConfigurationStatusUrlConfiguration("GOOG", "PROD", "https://google.com"));
 
@@ -41,9 +50,30 @@ public class HealthCheckApplication {
 	@RequestMapping(path ="/health-check")
 	public String healthCheck() throws IOException {
 
-        RestTemplate restTemplate =new RestTemplate();
+        /* RestTemplate restTemplate =new RestTemplate
+        (); */
 
-        return "OK";
+        ConfigurationStatusUrlConfiguration getComponentDetail = this.getComponentDetails();
+        StringBuilder status =new StringBuilder("Component : ");
+
+        status.append(getComponentDetail.getComponentId());
+        status.append(" ");
+        status.append(" Status");
+
+        try {
+            HttpsURLConnection httpsConnection = httpsConnectionFactory.getHttpsConnection(getComponentDetail.getUrl());
+            InputStream inputStream = httpsConnection.getInputStream();
+            //set timeout for socket and read as we can not wait till end
+            //todo read json from manifest response
+            status.append(httpsConnection.getResponseCode());
+
+        } catch (Exception e) {
+            status.append("NOK");
+
+        }
+
+
+        return status.toString();
 
     }
 
